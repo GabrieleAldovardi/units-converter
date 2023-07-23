221c538dc1e79c4675c160a1a6ac9604b575c7c1
Gabriele Aldovardi <gabriele.aldovardi@gmail.com>
2023-07-23 10:47:52 +0200

    insert all the units

--- src/main/java/com/example/generalconverter/ConverterController.java
+++ src/main/java/com/example/generalconverter/ConverterController.java
@@ -1,27 +1,37 @@
 package com.example.generalconverter;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConverterController {
-    @FXML private Label startUnitLabel, endUnitLabel, infoLabel, searchLabel;
-    @FXML private TextField startUnitField, endUnitField, searchUnitField;
-    @FXML private Button convertButton;
-    @FXML private ChoiceBox<String> conversionBox;
+    @FXML private Label startUnitLabel, endUnitLabel, searchStartLabel, searchFianlLabel;
+    @FXML private TextField startUnitField, finalUnitField, searchStartUnitField, searchFinalUnitField;
+    @FXML private Button convertButton, aboutButton;
+    @FXML private ComboBox<String> startUnitBox, finalUnitBox;
+    private final static List<String> temperatures = Arrays.asList("Celsius", "Fahrenheit", "Kelvin");
+    private final static List<String> distances = Arrays.asList("Meter", "Kilometer", "Centimeter", "Millimeter", "Feet", "Yard",
+            "Inch", "Mile");
+    private final static List<String> weights = Arrays.asList("Gram", "Milligram", "Kilogram", "Ounce", "Pound", "Ton");
+    private final static List<String> volumes = Arrays.asList("Litre", "Millilitre",
+            "Kilolitre", "Gallon", "Pint", "Fluid Ounce");
+    private final static List<String> times = Arrays.asList("Second", "Minute", "Hour", "Day", "Month", "Week", "Year");
+    private final static List<String> units = new ArrayList<>();
     @FXML public void initialize() {
-        conversionBox.getItems().addAll("C -> F", "F -> C");
-        conversionBox.getSelectionModel().select("C -> F");
+        units.addAll(temperatures);
+        units.addAll(distances);
+        units.addAll(weights);
+        units.addAll(volumes);
+        units.addAll(times);
+
+        startUnitBox.getItems().addAll(units);
+        startUnitBox.setVisibleRowCount(10);
+        finalUnitBox.getItems().addAll(units);
+        finalUnitBox.setVisibleRowCount(10);
     }
-    /*@FXML void handleConvert(ActionEvent event) {
-        if(conversionBox.getSelectionModel().getSelectedItem().equals("C -> F")) {
-            Double f = Double.parseDouble(celsiusField.getText()) * 1.8 + 32;
-            fahrenheitField.setText(f.toString());
-        } else {
-            Double c = (Double.parseDouble(fahrenheitField.getText()) - 32) / 1.8;
-            celsiusField.setText(c.toString());
-        }
-    }*/
+
+
 }
