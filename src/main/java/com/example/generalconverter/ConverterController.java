0a79931cb265fce5d107d8fd1b9d4b5333b201ea
Gabriele Aldovardi <gabriele.aldovardi@gmail.com>
2023-07-27 14:15:56 +0200

    Modified the UI, added more units and implemented the research

--- src/main/java/com/example/generalconverter/ConverterController.java
+++ src/main/java/com/example/generalconverter/ConverterController.java
@@ -1,37 +1,66 @@
 package com.example.generalconverter;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Map.entry;
 
 public class ConverterController {
-    @FXML private Label startUnitLabel, endUnitLabel, searchStartLabel, searchFianlLabel;
-    @FXML private TextField startUnitField, finalUnitField, searchStartUnitField, searchFinalUnitField;
+    @FXML private TextField startUnitField, finalUnitField, searchStartUnitField;
     @FXML private Button convertButton, aboutButton;
     @FXML private ComboBox<String> startUnitBox, finalUnitBox;
-    private final static List<String> temperatures = Arrays.asList("Celsius", "Fahrenheit", "Kelvin");
-    private final static List<String> distances = Arrays.asList("Meter", "Kilometer", "Centimeter", "Millimeter", "Feet", "Yard",
-            "Inch", "Mile");
-    private final static List<String> weights = Arrays.asList("Gram", "Milligram", "Kilogram", "Ounce", "Pound", "Ton");
-    private final static List<String> volumes = Arrays.asList("Litre", "Millilitre",
-            "Kilolitre", "Gallon", "Pint", "Fluid Ounce");
-    private final static List<String> times = Arrays.asList("Second", "Minute", "Hour", "Day", "Month", "Week", "Year");
-    private final static List<String> units = new ArrayList<>();
+    private final List<String> temperatures = Arrays.asList("Celsius", "Fahrenheit", "Kelvin");
+    private final List<String> angles = Arrays.asList("Degrees", "Radiant");
+    private final List<String> distances = Arrays.asList("Kilometer", "Meter", "Decimeter", "Centimeter", "Millimeter", "Micrometer", "Nanometer", "Feet", "Yard", "Inch", "Mile", "Light year");
+    private final List<String> weights = Arrays.asList("Kilogram", "Hectogram", "Gram", "Centigram", "Milligram",
+            "Ounce", "Pound", "Ton");
+    private final List<String> volumes = Arrays.asList("Kilolitre", "Litre", "Decilitre", "Centilitre", "Millilitre",
+            "Gallon", "Pint", "Fluid Ounce", "Cubic decimeter", "Cubic centimeter", "Cubic milliliter");
+    private final List<String> times = Arrays.asList("Nanosecond", "Microsecond", "Millisecond", "Second", "Minute",
+            "Hour", "Day", "Month", "Week", "Year");
+    private final List<String> velocity = Arrays.asList("Kilometer per second", "Kilometer per hour", "Meter per " +
+            "second", "Meter per hour", "Miles per second", "Miles per hour", "Light speed", "Sound speed");
+    private final List<String> currency = Arrays.asList("Argentinian-Pesos", "Dirham", "Euro", "Indian-Rupee", "Naira",
+            "Pound", "US-dollar",  "Won",  "Yen", "Yuan");
+    private final List<String> data = Arrays.asList("Bit", "Byte", "Kilobit", "Kilobyte","Megabit","Megabyte",
+            "Gigabit", "Gigabyte", "Terabit", "Terabyte", "Petabit", "Petabyte", "Exabit", "Exabyte");
+    private final List<List<String>> allUnits = List.of(temperatures, angles, distances, weights, volumes, times,
+            velocity, currency, data);
     @FXML public void initialize() {
-        units.addAll(temperatures);
-        units.addAll(distances);
-        units.addAll(weights);
-        units.addAll(volumes);
-        units.addAll(times);
-
+        List<String> units =
+                new ArrayList<String>(allUnits.stream().flatMap(Collection::stream).collect(Collectors.toList()));
         startUnitBox.getItems().addAll(units);
         startUnitBox.setVisibleRowCount(10);
-        finalUnitBox.getItems().addAll(units);
-        finalUnitBox.setVisibleRowCount(10);
     }
 
+    @FXML public void handleStartBox() {
+        int index;
+        for(index = 0; index < allUnits.size(); index++)
+            if (allUnits.get(index).contains(startUnitBox.getValue()))
+                break;
+        finalUnitBox.getItems().clear();
+        finalUnitBox.getItems().addAll(allUnits.get(index));
+        finalUnitBox.getItems().remove(startUnitBox.getValue());
+        finalUnitBox.setVisibleRowCount(5);
+    }
 
+    @FXML public void handleStartSearch() {
+        if(startUnitBox.getItems().contains(searchStartUnitField.getCharacters().toString())) {
+            startUnitBox.setValue(searchStartUnitField.getCharacters().toString());
+            searchStartUnitField.clear();
+        } else {
+            Alert wrongUnit = new Alert(Alert.AlertType.ERROR);
+            wrongUnit.setTitle("Error");
+            wrongUnit.setHeaderText("Wrong unit typed");
+            wrongUnit.showAndWait();
+        }
+    }
+    @FXML public void handleConvert() {
+
+    }
 }
